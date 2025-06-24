2d7a36778bac6dc69b560812ec5a6967116cd045
Edward Minaya <edmiya.dev@gmail.com>
2025-06-24 19:06:24 -0400

    swaggerUi added

--- ApiEcommerce/Program.cs
+++ ApiEcommerce/Program.cs
@@ -13,13 +13,13 @@ builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(pattern:"swagger/openapi/v1.json");
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/openapi/v1.json", "ApiEcommerce v1"));
 }
 
 app.UseHttpsRedirection();
